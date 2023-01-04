86bc1a8e9deebff3c91951c859e36990bffb0e43
I'm <51687020+Sayan-Alu@users.noreply.github.com>
2023-01-04 08:48:52 -0800

    Add files via upload
    a function to calculate the total sum the numbers in a list.
    edyoda assignment for python

---
+++ 6_a_assmnt.py
@@ -0,0 +1,14 @@
+#=============================================================
+#a function to calculate the total sum the numbers in a list
+#=============================================================
+
+def total(list1):
+    a = 0
+    for i in list1:
+         a = a+i
+    print("sum of list is :" , a)    
+                
+   
+
+
+total([1,2,3,4,10])
