d8f6550572f73233fce217f3e88a1f0cfbf7d3e8
I'm <51687020+Sayan-Alu@users.noreply.github.com>
2023-01-04 08:50:43 -0800

    Add files via upload
    #===========================================
    #PYTHON FUNCTION TO REVERSE A STRING
    #===========================================

---
+++ 7th_assmnt.py
@@ -0,0 +1,14 @@
+#===========================================
+#PYTHON FUNCTION TO REVERSE A STRING
+#===========================================
+print("================================")
+
+a= str(input('please type a string ::>>'))
+def reversingg(a):
+
+    print(f"after reversing --  {a[-1::-1]}")
+
+
+reversingg(a)
+
+print("==========================================")
