0dcb8a45667508df7d536d10c230d29b501411ae
I'm <51687020+Sayan-Alu@users.noreply.github.com>
2023-01-09 09:54:18 -0800

    ✔✔ASSIGNMENT_5-EDYODA
    a python programme to print a dictionary whose keys should be the alphabet from a-z 
     and the value should be corresponding ASCII values..   . ..  ..  .. .,,,...,,.,..,,.,.,..,.,,.,,..,...

---
+++ make ur own dict.py
@@ -0,0 +1,27 @@
+
+
+
+#for i in range(97,123):
+ #  print((chr(i)), ":",(int(i)))"""
+
+print("SAYAN's_ASSIGNMENT", end="\n\n")
+print("=================================", end="\n\n")
+
+
+
+a = dict()                                                  #          a python progm to print a dictionary whode keys should be the alphabet from a-z 
+                                                            #          and the value should be corresponding ASCII values
+for i in range(97,123):
+    a[chr(i)] = int(i)
+print(a, end="\n\n\n")   
+print('===============================') 
+
+# nxt one is just a practise....
+for i in range(97,123):
+   print("{",(chr(i)), ":",(int(i)),"}",end=" ")
+
+   
+   
+              
+                 #
+
