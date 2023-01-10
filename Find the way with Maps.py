cd686c71998d6a1aba0fd46a90ae3f3a089844f7
I'm <51687020+Sayan-Alu@users.noreply.github.com>
2023-01-10 03:51:04 -0800

    ASSIGNMENT_EDYODA_QS-no_10
    a Python program to triple all numbers of a given list of integers. Use Python map.

---
+++ Find the way with Maps.py
@@ -0,0 +1,13 @@
+
+print("===========================================================",end="\n\n")
+
+lst = eval(input('type a list...', ))
+
+
+#lst = [2,5,7,4,9]
+sayan = list(map(lambda x : x*3 , lst))
+
+print(end="\n\n")
+print('multiplied list is :' , sayan,end="\n\n")
+
+print("===============================================SAYAN=======")
