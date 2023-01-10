54b76fb744d2c65048967f891ee5da2ce717479c
I'm <51687020+Sayan-Alu@users.noreply.github.com>
2023-01-10 02:12:59 -0800

    ASSIGNMENT_QS-no : 9
    a Python program to create a lambda function that adds 25 to a given number passed in as an argument.

---
+++ Play with Lambda.py
@@ -0,0 +1,18 @@
+
+                   #  a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+
+print("======================================================================")
+
+x = lambda a: a + 25
+y = x(eval(input("enter the number : " )))
+print("the output is : ", y) 
+
+
+print("================================================SAYAN===================")
+
+
+
+
+
+
